8a0b885a89352190641acae4dd145d0ace167eb2
Jorge052011 <jimunozacuna+1@gmail.com>
2025-07-25 21:50:48 +0000

    W

--- mi_app/views.py
+++ mi_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Producto
+from django.views.generic import ListView
+
+
 # Create your views here.
 
 def hola(request):
@@ -9,3 +12,9 @@ def listar_productos(request):
     productos = Producto.objects.all()
 
     return render(request, "mi_app/lista_de_productos.html", {"productos":productos})
+
+
+class ProductoListView(ListView):
+    model = Producto
+
+
